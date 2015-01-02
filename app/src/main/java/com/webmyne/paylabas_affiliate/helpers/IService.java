e380ae390d255325e7da40aaeb89470be6c59c50
Android <dd.droid@live.com>
2015-01-02 11:47:29 +0530

    ng

---
+++ app/src/main/java/com/webmyne/paylabas_affiliate/helpers/IService.java
@@ -0,0 +1,10 @@
+package com.webmyne.paylabas_affiliate.helpers;
+
+import com.android.volley.VolleyError;
+
+public interface IService {
+	
+	public void response(String response);
+	public void error(VolleyError error);
+
+}
