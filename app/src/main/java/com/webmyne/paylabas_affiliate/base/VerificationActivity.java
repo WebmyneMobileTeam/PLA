4dae0bd2dd8cb6b2143d974a55a732ab752899e7
Nirav <niravnvk@gmail.com>
2015-01-02 18:44:34 +0530

    aa

--- app/src/main/java/com/webmyne/paylabas_affiliate/base/VerificationActivity.java
+++ app/src/main/java/com/webmyne/paylabas_affiliate/base/VerificationActivity.java
@@ -6,28 +6,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
 import android.widget.Button;
+
+
 import com.webmyne.paylabas_affiliate.R;
 
+public class VerificationActivity extends ActionBarActivity {
 
-public class Launcher extends ActionBarActivity {
-
-    private Button btnLoginNext;
+    private Button btnFinishSetup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
-        btnLoginNext= (Button) findViewById(R.id.btnLoginNext);
-        btnLoginNext.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_verification);
+        btnFinishSetup= (Button) findViewById(R.id.btnFinishSetup);
+        btnFinishSetup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(Launcher.this,VerificationActivity.class);
+                Intent intent =new Intent(VerificationActivity.this,MyDrawerActivity.class);
                 startActivity(intent);
             }
         });
     }
-
 
 
 }
